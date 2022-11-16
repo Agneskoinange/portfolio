1874b04598df0869dd9306bf0944b5bbe2dd5a18
Agnes <koinageagness@gmail.com>
2022-11-16 09:37:26 +0300

    General styling section

--- js/app.js
+++ js/app.js
@@ -104,3 +104,25 @@ let mixer = mixitup(".portfolio-gallery", {
 });
 
 /*--------Page theme------ */
+
+let firstTheme = localStorage.getItem("dark")
+
+changeTheme(+firstTheme);
+
+function changeTheme(){
+
+    if (isDark){
+        document.body.classList.add("dark");
+        toggle_btn.classList.replace("uil-moon", "uil-sun");
+        localStorage.setItem("dark", 1);
+    } else {
+        document.body.classList.add("dark");
+        toggle_btn.classList.replace("uil-sun", "uil-moon");
+        localStorage.setItem("dark", 0);
+
+    }
+}
+
+toggle_btn.addEventListener("click", () => {
+    changeTheme((!document.body.classList.contains("dark")));
+});
